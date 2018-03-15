a96e1040b6a0d0339467840c214a1d0e6c028c02
Yenne McClellan <Cheyenne@bills-MacBook-Pro-2.local>
2018-03-15 15:53:37 -0700

    jbadvkl

--- project2/main.js
+++ project2/main.js
@@ -3,5 +3,6 @@ console.log(hashTagWeird)
 
 hashTagWeird.addEventListener('click',function(){
   hashTagWeird.classList.add('beenClicked')
-  document.getElementById('worm').style.color ="gray";
+  document.getElementById('worm').style.color ="#A9A9A9";
+  document.getElementById("bigworm").style.visibility= "visible";
 })
